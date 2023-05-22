dfba4972735166b34ae4da4bc324223f951432a8
Muhammad Talha <talhabinay@gmail.com>
2023-05-22 05:50:02 +0500

    update

--- Client/src/PageComponents/ErrorPage.js
+++ Client/src/PageComponents/ErrorPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 const ErrorPage = () => {
   return (
-    <div className="w-full h-screen flex-col flex justify-center items-center">
+    <div className="px-10 w-full h-screen flex-col flex justify-center items-center">
       <h1 className="font-bold text-5xl">OOPS!</h1>
       <p className="my-3 text-lg font-semibold">404 - PAGE NOT FOUND</p>
       <i>
